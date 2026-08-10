c95fb99ab2f093c7f85f607f9d3b9e3296ce04e7
Joel N <joelmanjaly2011@gmail.com>
2026-08-10 12:19:21 +0400

    Update commands.rs
    Signed-off-by: Joel N <joelmanjaly2011@gmail.com>

--- crates/pumpkin-protocol/src/java/client/play/commands.rs
+++ crates/pumpkin-protocol/src/java/client/play/commands.rs
@@ -288,7 +288,10 @@ impl ArgumentType {
         version: &JavaMinecraftVersion,
     ) -> Result<(), WritingError> {
         let id = self.to_id(version);
-        eprintln!("Writing ArgumentType {self:?} as id {id} for version {version:?}");
+        #[allow(clippy::print_stderr)]
+        {
+            eprintln!("Writing ArgumentType {self:?} as id {id} for version {version:?}");
+        }
         write.write_var_int(&(id).into())?;
         match self {
             Self::Float { min, max } => Self::write_number_arg(*min, *max, write),
